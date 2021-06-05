d2ddf54fda0747739575097557884c54340e3eac
Maxim Burakov <bmaximxx@gmail.com>
2021-06-05 18:52:16 +0300

    added admin dashboard urls

--- app/admin_views.py
+++ app/admin_views.py
@@ -3,4 +3,24 @@ from flask import render_template
 
 @app.route("/admin/dashboard")
 def admin_dashboard():
+    return render_template("admin/station.html")
+
+@app.route("/admin/dashboard/add")
+def admin_dashboard2():
     return render_template("admin/dashboard.html")
+
+
+@app.route("/admin/dashboard/guns")
+def admin_guns():
+    return render_template("admin/guns.html")
+
+
+@app.route("/admin/dashboard/station")
+def admin_station():
+    return render_template("admin/station.html")
+
+
+@app.route("/admin/dashboard/people")
+def admin_people():
+    return render_template("admin/people.html")
+
